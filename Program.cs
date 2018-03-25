1882b52fb9aef59d2625bd8a2154a9f5054e7e37
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-03-25 23:23:17 +1300

    inital commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace carClass
+{
+    public class Car
+    {
+        private string colour; 
+        public Car(string col)
+        {
+            colour = col; 
+        }
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+        string make {get; set;}
+        string gearbox {get; set;} 
+
+        class Program
+        {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+}
